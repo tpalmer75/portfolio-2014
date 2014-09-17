a7264d5a959a24db4205f21d631cae7ec73de656
tpalmer75 <taylor.hatch.palmer@gmail.com>
2014-09-17 12:50:53 -0600

    New portfolio template, changed Jade script structure

--- js/main.js
+++ js/main.js
@@ -16,48 +16,6 @@
     else document.documentElement.className += ' no-svg'
 }()
 
-
-// ------ SAFARI SIZING FIX -------
-
-// Detect Safari Browsers except for Chrome
-if (navigator.userAgent.indexOf('Safari') != -1 && !navigator.userAgent.match(/(Chrome)/) ) { 
-
- 
-  // Safari Size Function
-  var safariSize = function() {
-       
-    var page = $('#page');
-   
-    // Strip inline width
-    page.css('width', '');
-    
-    // Find page width
-    var pageWidth = $('#page').width();
-    // Finding the nearest width divisible by 12, add 13 for gap
-    var safariWidth = Math.floor(pageWidth / 12) * 12 + 13;
-
-    // If the page is not divisible by 12, make new width
-    page.css('width', safariWidth);
-    
-  };
-
-  // Run function on resize
-  $(window).resize(safariSize);
-  // Trigger resize on load to run safariSize function
-  $(window).trigger($.Event('resize'));
-}
-
-
-
-// Masonry
-
-var $container = $('#page');
-$container.masonry({
-  columnWidth: '.grid-sizer',
-  itemSelector: '.item'
-});
-
-
 // Menu button
 
 $('#menu-button').click(function() {
